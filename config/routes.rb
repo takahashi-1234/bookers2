bb7a20f2dfd952bf48ed539b529b186bbfc36b7f
EC2 Default User <riki.1130@icloud.com>
2021-10-11 06:48:37 +0000

    [update]検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
     get 'followings' =>'relationships#followings' ,as:'followings'
     get 'followers' => 'relationships#followers' ,as:'followers'
   end
+  get '/search', to: 'searchs#search'
 
 end
